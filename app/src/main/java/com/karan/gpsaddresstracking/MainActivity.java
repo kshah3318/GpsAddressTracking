eb1ec525020592c786359ad107198f5b1fdaee3f
kshah3318 <kshah3318@gmail.com>
2017-07-07 18:51:29 +0530

    GpsTracking Address from Latitude and Longtitude

--- app/src/main/java/com/karan/gpsaddresstracking/MainActivity.java
+++ app/src/main/java/com/karan/gpsaddresstracking/MainActivity.java
@@ -1,13 +1,39 @@
 package com.karan.gpsaddresstracking;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvAddress;
+    AppLocationService appLocationService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvAddress=(TextView)findViewById(R.id.txtDispAddress);
+        appLocationService=new AppLocationService(this);
+        if(appLocationService.canGetLocation())
+        {
+            double latitude=appLocationService.getLatitude();
+            double longtitude=appLocationService.getLongitude();
+            Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longtitude
+                    , Toast.LENGTH_LONG).show();
+            tvAddress.setText(appLocationService.getAddressLineOne(this)+appLocationService.getLocality(this)+appLocationService.getAddressLineTwo(this));
+        }
+        else
+        {
+            appLocationService.showSettingsAlert();
+        }
     }
 }
+
